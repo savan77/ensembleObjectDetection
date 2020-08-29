8bcc10acf81ea0f49a81a6da5b28f4563298149a
Savan Visalpara <savan77@users.noreply.github.com>
2020-08-28 19:10:36 -0500

    use pip instead of pip3

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/fizyr/keras-retinanet
-pip3 install ./keras-retinanet/ --user
+pip install ./keras-retinanet/ --user
 git clone https://github.com/onepanelio/Mask_RCNN
-pip3 install ./Mask_RCNN/ --user
+pip install ./Mask_RCNN/ --user
 wget https://www.dropbox.com/sh/n21kckhsi200b52/AABxspis34aAZiMUp_cQ6RYFa?dl=1 -O weights.zip
 unzip weights.zip -d /mnt/weights/
-pip3 install -r requirements.txt
+pip install -r requirements.txt
