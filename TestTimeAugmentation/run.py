17e705c80ac4d5b971c104b908373c2ec11a3b22
Savan Visalpara <savan77@users.noreply.github.com>
2020-08-31 16:32:55 -0500

    update dirs

--- TestTimeAugmentation/run.py
+++ TestTimeAugmentation/run.py
@@ -17,24 +17,24 @@ def main(args):
     models_list = args.models.split(",")
     print("Models to be run: ", models_list)
     if 'mask_rcnn' in models_list:
-        maskRcnn = testTimeAugmentation.MaskRCNNPred('/mnt/weights/mask_rcnn_coco.h5', '/mnt/weights/coco.names')
+        maskRcnn = testTimeAugmentation.MaskRCNNPred('/mnt/src/mask_rcnn_coco.h5', '/mnt/src/coco.names')
         listModels.append(maskRcnn)
     if 'retinanet' in models_list:
-        retinaResnet50 = testTimeAugmentation.RetinaNetResnet50Pred('/mnt/weights/resnet50_coco_best_v2.1.0.h5', '/mnt/weights/coco.csv')
+        retinaResnet50 = testTimeAugmentation.RetinaNetResnet50Pred('/mnt/src/resnet50_coco_best_v2.1.0.h5', '/mnt/src/coco.csv')
         listModels.append(retinaResnet50)
     if 'yolo_darknet' in models_list:
-        yoloDarknet = testTimeAugmentation.DarknetYoloPred('/mnt/weights/yolov3.weights', '/mnt/weights/coco.names','/mnt/weights/yolov3.cfg')
+        yoloDarknet = testTimeAugmentation.DarknetYoloPred('/mnt/src/yolov3.weights', '/mnt/src/coco.names','/mnt/src/yolov3.cfg')
         listModels.append(yoloDarknet)
     if 'ssd_resnet' in models_list:
-        ssdResnet = testTimeAugmentation.MXnetSSD512Pred('/mnt/weights/ssd_512_resnet50_v1_voc-9c8b225a.params', '/mnt/weights/classesMXnet.txt')
+        ssdResnet = testTimeAugmentation.MXnetSSD512Pred('/mnt/src/ssd_512_resnet50_v1_voc-9c8b225a.params', '/mnt/src/classesMXnet.txt')
         listModels.append(ssdResnet)
     if 'faster_resnet' in models_list:
-        fasterResnet = testTimeAugmentation.MXnetFasterRCNNPred('/mnt/weights/faster_rcnn_resnet50_v1b_voc-447328d8.params', '/mnt/weights/classesMXnet.txt')
+        fasterResnet = testTimeAugmentation.MXnetFasterRCNNPred('/mnt/src/faster_rcnn_resnet50_v1b_voc-447328d8.params', '/mnt/src/classesMXnet.txt')
         listModels.append(fasterResnet)
         
 #     listaModels = [retinaResnet50, maskRcnn]
     models(listModels,args.images_path,args.option, args.combine)
-    print(os.listdir('/mnt/weights/'))
+    print(os.listdir('/mnt/src/'))
     
 
 if __name__ == "__main__":
