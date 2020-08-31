90ab8e0337754fc92503cee7834630cb670e1315
Savan Visalpara <savan77@users.noreply.github.com>
2020-08-31 16:32:09 -0500

    change dir for models

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ pip install ./keras-retinanet/ --user
 git clone https://github.com/onepanelio/Mask_RCNN
 pip install ./Mask_RCNN/ --user
 wget https://www.dropbox.com/sh/n21kckhsi200b52/AABxspis34aAZiMUp_cQ6RYFa?dl=1 -O weights.zip
-unzip weights.zip -d /mnt/weights/
+unzip weights.zip -d /mnt/src/
 pip install -r requirements.txt
