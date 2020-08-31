93e1d8356b63557adea96fa9d16c10f6b3d69421
Savan Visalpara <savan77@users.noreply.github.com>
2020-08-31 17:22:24 -0500

    added ls command

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ git clone https://github.com/onepanelio/Mask_RCNN
 pip install ./Mask_RCNN/ --user
 wget https://www.dropbox.com/sh/n21kckhsi200b52/AABxspis34aAZiMUp_cQ6RYFa?dl=1 -O weights.zip
 unzip weights.zip -d /mnt/src/
+ls
 pip install -r requirements.txt
