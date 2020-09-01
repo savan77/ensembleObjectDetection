a5bcac0b11dae100df9d274801749b6ace6b249b
Savan Visalpara <savan77@users.noreply.github.com>
2020-08-31 20:16:53 -0500

    update moving logic

--- TestTimeAugmentation/mainModel.py
+++ TestTimeAugmentation/mainModel.py
@@ -39,13 +39,13 @@ def models(listaModels,pathImg,option, combine=False):
 
         list_dir = os.listdir(pathImg+'/../salida')
         dest = "/mnt/output/"
-
-        for sub_dir in list_dir:
-            print("sub dir:", sub_dir)
-            for file in os.listdir(os.path.join(pathImg+'/../salida',sub_dir)):
-                print("file:", file)
-                dir_to_move = os.path.join(pathImg+'/../salida', sub_dir, file)
-                shutil.move(dir_to_move, os.path.join(dest, sub_dir, file))
+        shutil.move(pathImg+'/../salida/', '/mnt/output')
+#         for sub_dir in list_dir:
+#             print("sub dir:", sub_dir)
+#             for file in os.listdir(os.path.join(pathImg+'/../salida',sub_dir)):
+#                 print("file:", file)
+#                 dir_to_move = os.path.join(pathImg+'/../salida', sub_dir, file)
+#                 shutil.move(dir_to_move, os.path.join(dest, sub_dir, file))
 
     else:
         # 5. We perform the ensemble method
